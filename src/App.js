95844872ccb1b5c9a2c1d7e43dad08d99062baf8
mor bargig <morbargig@gmail.com>
2019-11-05 03:55:04 +0200

    hebrew

--- src/App.js
+++ src/App.js
@@ -191,7 +191,7 @@ class App extends Component {
           </a>}
       </div>
       {this.state.isMobile ? <div id="pdf">
-        <iframe src={this.state.url} width="100%" height="100%" style={{ width: 375 + 'px', height: 812 + 'px' }} frameborder="0" scrolling="yes">
+        <iframe src={this.state.url} width="100%" height="100%" style={{ width: 375 + 'px', height: 812 + 'px' }} frameborder="0" scrolling="no">
           <p>It appears your web browser doesn't support iframes.</p>
         </iframe>
         {/* <object data="lorem.pdf" type="application/pdf">
